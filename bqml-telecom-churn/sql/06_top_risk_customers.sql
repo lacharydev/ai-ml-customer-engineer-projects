5e9729de03c0c0119bb301e011ecc5a8cda3a615
lacharydev <lachary.dev@gmail.com>
2025-10-17 12:26:34 -0700

    Add SQL query to find top risk customers
    This query retrieves the top 20 customers with the highest probability of churning based on the churn model predictions.

--- bqml-telecom-churn/sql/06_top_risk_customers.sql
+++ bqml-telecom-churn/sql/06_top_risk_customers.sql
@@ -1,1 +1,18 @@
+-- 06_top_risk_customers.sql
+-- Get the top customers most likely to churn
+
+SELECT
+  customerID,
+  probs['Yes'] AS churn_probability
+FROM (
+  SELECT
+    customerID,
+    predicted_Churn_probs AS probs
+  FROM ML.PREDICT(
+    MODEL `learned-pact-475307-g8.my_datasets.churn_model`,
+    TABLE `learned-pact-475307-g8.my_datasets.telecom_churn_trainview`
+  )
+)
+ORDER BY churn_probability DESC
+LIMIT 20;
 
