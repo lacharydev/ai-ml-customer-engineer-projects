7e6bde236f10fb690d8db1a16d37ce44f3790e2d
lacharydev <lachary.dev@gmail.com>
2025-10-17 02:05:33 -0700

    Add files via upload

--- bqml-telecom-churn/sql/01_explore_data.sql
+++ bqml-telecom-churn/sql/01_explore_data.sql
@@ -1,1 +1,14 @@
+-- 01_explore_data.sql
+-- Total rows in the dataset
+SELECT COUNT(*) AS total_rows
+FROM `learned-pact-475307-g8.my_datasets.telecom_churn`;
 
+-- Churn distribution
+SELECT Churn, COUNT(*) AS cnt
+FROM `learned-pact-475307-g8.my_datasets.telecom_churn`
+GROUP BY Churn;
+
+-- Average charges by churn
+SELECT Churn, AVG(MonthlyCharges) AS avg_monthly, AVG(tenure) AS avg_tenure
+FROM `learned-pact-475307-g8.my_datasets.telecom_churn`
+GROUP BY Churn;
