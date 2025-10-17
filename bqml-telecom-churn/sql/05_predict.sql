8f31b7bb8e98b67aad6ab2707e1ba9d900869359
lacharydev <lachary.dev@gmail.com>
2025-10-17 12:26:04 -0700

    Add churn prediction SQL query
    Added SQL query to predict churn outcomes for customers.

--- bqml-telecom-churn/sql/05_predict.sql
+++ bqml-telecom-churn/sql/05_predict.sql
@@ -1,1 +1,13 @@
+-- 05_predict.sql
+-- Predict churn outcomes for all customers
+
+SELECT
+  customerID,
+  predicted_Churn,
+  predicted_Churn_probs
+FROM ML.PREDICT(
+  MODEL `learned-pact-475307-g8.my_datasets.churn_model`,
+  TABLE `learned-pact-475307-g8.my_datasets.telecom_churn_trainview`
+)
+LIMIT 20;
 
